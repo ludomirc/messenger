09611dee2a2f791dd0280a9157c00f505216110e
beniamin <beniamin.czaplicki@gmail.com>
2019-10-12 22:07:00 +0200

    added UserDto

--- messenger-post/src/main/java/org/qbit/messanger/post/dto/UserDto.java
+++ messenger-post/src/main/java/org/qbit/messanger/post/dto/UserDto.java
@@ -7,15 +7,10 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Data
-public class PostDto implements Serializable {
-
-    private Long id;
+@AllArgsConstructor
+public class UserDto implements Serializable {
 
     @NotBlank
     @Size(min = 1, max = 50)
     private String userId;
-
-    @NotBlank
-    @Size(min = 1, max = 140)
-    private String body;
 }
